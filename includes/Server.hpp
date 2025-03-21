d1715302881b7bb48b991a03329ad538096163e1
Yulia Ioffe <yumioffe@gmail.com>
2025-03-21 14:05:07 +0000

    use parsing results

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -7,14 +7,16 @@ class Server {
    private:
     int _socketFd;
     int _port;
+    std::string _ip;
 
     // just for the sake of orthodox form, not really needed
+    Server();
     Server(const Server& other);
     Server& operator=(const Server& other);
     void acceptClient();
 
    public:
-    Server();
+    Server(int port, std::string ip);
     ~Server();
 
     void start();
