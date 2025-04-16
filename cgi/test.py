745160e2d32d577759b6c9c97ce15963c54f48d5
Yulia Ioffe <yumioffe@gmail.com>
2025-04-16 14:02:04 +0100

    extend example

--- cgi/test.py
+++ cgi/test.py
@@ -2,3 +2,10 @@
 
 print("Content-Type: text/html\n")
 print("<html><body>Hello from Python CGI</body></html>")
+
+# import os
+
+# print("Content-Type: text/plain\n")
+# print("CGI Environment Variables:")
+# for key in sorted(os.environ):
+#     print(f"{key}: {os.environ[key]}")
